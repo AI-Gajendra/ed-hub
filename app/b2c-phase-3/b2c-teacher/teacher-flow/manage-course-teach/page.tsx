5e4a22cd50589ec9fefec8ce74c7be4fcf5d50c9
tanushka <tanushka1726@gmail.com>
2025-07-24 13:52:03 +0530

    edit teacher-b2c-phase-3

--- app/b2c-phase-3/b2c-teacher/teacher-flow/manage-course-teach/page.tsx
+++ app/b2c-phase-3/b2c-teacher/teacher-flow/manage-course-teach/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/TeacherB2CHeader";
 import GoBack from "@/components/principal/goback";
-import TeachingForm from "@/components/teacher-b2c/manage-course-teach";
+import TeachingForm from "./manage-course-teach";
 
 const ManageTeachCourse = () => {
 
